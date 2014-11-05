5dd1990d9aa634b3514964cd4f311618614f57e5
Jimmy C. Chau <jchau@bu.edu>
2014-11-05 13:50:09 -0500

    Updated grid-single-node-matlab.sh for MATLAB R2014a and R2014b.
    Also edited script documentation/comments.
    
    Since MATLAB R2014a, the number of local workers is no longer limited to 12.
    
    Also, MATLAB now uses parpool (instead of matlabpool) to start the parallel workers.
    
    For now, the script specifically calls MATLAB R2014b (since MATLAB R2013a is currently the default MATLAB on the SCC and MATLAB R2013a does not support the parpool command).

--- grid-single-node-matlab.sh
+++ grid-single-node-matlab.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 # grid-single-node-matlab.sh: Generic script for use with qsub to start a
 # parallel MATLAB job on a single node.
-# Written by Jimmy C. Chau <jchau@bu.edu> 2014 Oct 15-22
+# Written by Jimmy C. Chau <jchau@bu.edu> 2014 Oct 15-Nov 05
 
 # Usage (for BU SCC):
-#	qsub -N <jobname> -pe <parallel_env> <n> \
+#	qsub -N <jobname> [-pe <parallel_env> <n>] \
 #		<path>/grid-single-node-matlab.sh <matlabcmd>
 #
 # <parallel_env> is omp for parallel MATLAB scripts on SCC.
-# <n> is limited to <= 12 (until MATLAB 2014a).
+# <n> is limited to the number of processors available on the compute node.
+# If [-pe <parallel_env> <n>] is omitted then the job will only use one
+# processor.
 #
 # Note: execute from the directory in which the MATLAB script should run.
-# matlabcmd should be the MATLAB command to run after matlabpool.
-# If matlabcmd is the name of a script, the .m extension should be omitted.
+# <matlabcmd> should be the MATLAB command to run after matlabpool.
+# If <matlabcmd> is the name of a script, the .m extension should be omitted.
+#
+# Warning: grid-single-node-matlab.sh creates and uses a MATLAB variable
+# named 'cluster_gs'.  <matlabcmd> should avoid using this variable. 
+
+# The command used to start (a specific version or installation of) MATLAB.
+# >=matlab-2014a is required for the parpool command used in this script.
+MATLABCMD="/usr/local/apps/matlab-2014b/bin/matlab"
 
 # Job submission directives for qsub
 #$ -cwd
@@ -25,16 +34,15 @@ if [ -n "$NSLOTS" ]; then
 
 		# Starts MATLAB for a single worker.
 		# Skip running matlabpool (not necessary).
-		matlab -nodisplay -nosplash -r "$1, exit"
+		$MATLABCMD -nodisplay -nosplash -r "$1, exit"
 
 	elif [[ "$NSLOTS" -gt "1" ]]; then
 
 		# Starts MATLAB, fires up the parallel workers, and executes
-		# the specified MATLAB command(s) before closing the parallel
-		# workers and exiting.  
-		# Note: need JVM for matlabpool
-		matlab -nodisplay -nosplash -r \
-			"matlabpool open local $NSLOTS, $1, matlabpool close, exit"
+		# the specified MATLAB command(s) before exiting.  
+		# Note: JVM is needed for the Parallel Computing Toolbox.
+		$MATLABCMD -nodisplay -nosplash -r \
+			"cluster_gs = parcluster('local'); cluster_gs.NumWorkers=$NSLOTS; parpool(cluster_gs, cluster_gs.NumWorkers); $1, exit"
 
 	else
 		echo "Error: NSLOTS is invalid. Please only run using qsub." \
